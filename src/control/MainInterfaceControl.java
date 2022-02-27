cba2fa80f39e3936d202a934439bd0a49e359a43
ThanuraSilva <thanurasilva1989@gmail.com>
2022-02-27 23:49:44 +0530

    The main program functions optimized

--- src/control/MainInterfaceControl.java
+++ src/control/MainInterfaceControl.java
@@ -5,9 +5,13 @@ import com.jfoenix.controls.JFXToggleButton;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 
+import javax.management.StandardEmitterMBean;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,6 +38,7 @@ public class MainInterfaceControl {
     public JFXButton btnRun;
     public JFXToggleButton tglAdvancedMode;
     public TextField txtMainClass;
+    public Button btnOpen;
 
 
     public void initialize(){
@@ -47,6 +52,7 @@ public class MainInterfaceControl {
         MenuItem exit = new MenuItem("Exit");
         MenuItem select_all = new MenuItem("Select All");
         MenuItem open = new MenuItem("Open");
+        MenuItem wrapText = new MenuItem("Wrap Text");
 
         mnuBtnFile.getItems().add(open);
         mnuBtnFile.getItems().add(anew);
@@ -60,6 +66,40 @@ public class MainInterfaceControl {
         mnuBtnAbout.getItems().add(about);
 
         mnuBtnExtra.getItems().add(exit);
+        mnuBtnExtra.getItems().add(wrapText);
+
+
+        save.setOnAction(this::btnSave_OnAction);
+        copy.setOnAction(this::btnCopy_OnAction);
+        cut.setOnAction(this::btnCut_OnAction);
+        paste.setOnAction(this::btnPaste_OnAction);
+        select_all.setOnAction(this::btnSelectAll_OnAction);
+        open.setOnAction(this::btnOpen_OnAction);
+
+        wrapText.setOnAction(event -> {
+            txtInput.setWrapText(true);
+        });
+
+        exit.setOnAction(this::btnExit_OnAction);
+
+        //todo: new, save, about,exit,wrapText
+
+        txtInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            characterCount();
+        });
+
+    }
+
+    private void characterCount() {
+        String characters = txtInput.getText();
+        int count = 0;
+        for (int i = 0; i < characters.length(); i++) {
+            if (characters.charAt(i) != ' ') {
+                count++;
+            }
+
+        }
+        lblTotCharacters.setText(String.valueOf(count));
 
     }
 
@@ -68,36 +108,37 @@ public class MainInterfaceControl {
             lblModeSelect.setText("Advanced Mode Activated");
         }else {
             lblModeSelect.setText("Easy Mode Activated");
+
         }
     }
 
     public void btnRun_OnAction(ActionEvent actionEvent) throws IOException {
 
+        String main =txtMainClass.getText();
 
         if(lblModeSelect.getText().equals("Easy Mode Activated")){
 
-            String command = "public class " + txtMainClass.getText() + "{\n" +
+            String command = "public class " + main+ "{\n" +
                         "public static void main(String args[]){\n"+
                         txtInput.getText()+
                         "\n}\n" +
                         "}";
-            javaRun(command);
+            javaRun(command,main);
 
         }else if (lblModeSelect.getText().equals("Advanced Mode Activated")){
             String code = txtInput.getText();
-            javaRun(code);
+            javaRun(code,main);
 
         }
-
     }
 
-    private void javaRun(String command) throws IOException {
+    private void javaRun(String command,String main) throws IOException {
         try {
 
             //get Temp dir
 
             String tempDir = System.getProperty("java.io.tmpdir");
-            Path filePath = Paths.get(tempDir, txtMainClass.getText()+".java");
+            Path filePath = Paths.get(tempDir, main+".java");
             Files.write(filePath, command.getBytes());
 
             //file path to compiler
@@ -107,7 +148,7 @@ public class MainInterfaceControl {
             if (exitCode == 0){
                 //compile success and to be run
 
-                Process javaRun = Runtime.getRuntime().exec("java -cp " + tempDir +" "+txtMainClass.getText());
+                Process javaRun = Runtime.getRuntime().exec("java -cp " + tempDir +" "+main);
                 exitCode = javaRun.waitFor();
 
                 if (exitCode == 0) {
@@ -127,21 +168,14 @@ public class MainInterfaceControl {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }finally {
-            Path classPath = Paths.get(System.getProperty("java.io.tmpdir"), txtMainClass.getText()+".class");
-            Path javaPath = Paths.get(System.getProperty("java.io.tmpdir"), txtMainClass.getText()+".java");
+            Path classPath = Paths.get(System.getProperty("java.io.tmpdir"), main+".class");
+            Path javaPath = Paths.get(System.getProperty("java.io.tmpdir"), main+".java");
 
             Files.deleteIfExists(classPath);
             Files.deleteIfExists(javaPath);
 
         }
     }
-
-
-
-
-
-
-
 
 
     private void readStream(InputStream is) throws IOException {
@@ -153,20 +187,11 @@ public class MainInterfaceControl {
     }
 
     public void btnReset_OnAction(ActionEvent actionEvent) {
+        txtMainClass.clear();
+        txtInput.clear();
+        txtOutput.clear();
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public void btnCopy_OnAction(ActionEvent actionEvent) {
@@ -185,8 +210,44 @@ public class MainInterfaceControl {
         txtInput.selectAll();
     }
 
-    public void btnSave_OnAction(ActionEvent actionEvent) {
+    public void btnOpen_OnAction(ActionEvent actionEvent) {
+        try {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open the preferred file");
+        File file = fileChooser.showOpenDialog(null);
+        byte[] buffer = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+        txtInput.setText(new String(buffer));
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+       try {
+           FileChooser fileChooser = new FileChooser();
+           fileChooser.setTitle("Save the file");
+           fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("*.text","Text Files"));
+           File file = fileChooser.showSaveDialog(null);
+
+           Path path = Paths.get(file.getAbsolutePath());
+           byte[] buffer = txtInput.getText().getBytes();
+
+           OutputStream os = Files.newOutputStream(path);
+           os.write(buffer);
+           if(Files.exists(path)){
+               new Alert(Alert.AlertType.INFORMATION,"File Saved Successfully").show();
+           }else {
+               new Alert(Alert.AlertType.ERROR,"File Save failed.Try Again").show();
+           }
+           os.close();
+
+
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+
+       }
     }
 
     public void btnDelete_OnAction(ActionEvent actionEvent) {
@@ -195,24 +256,24 @@ public class MainInterfaceControl {
     }
 
     public void btnExit_OnAction(ActionEvent actionEvent) {
+        System.exit(0);
         //ToDo: develop as to function
        new Alert(Alert.AlertType.CONFIRMATION, "Are you sure to Exit,", ButtonType.OK, ButtonType.CANCEL).showAndWait();
 
 
     }
 
-    public void mnuBtnFile_OnAction(ActionEvent actionEvent) {
+    public void mnuBtnExtra_OnAction(ActionEvent actionEvent) {
     }
 
     public void mnuBtnEdit_OnAction(ActionEvent actionEvent) {
     }
 
-    public void mnuBtnExtra_OnAction(ActionEvent actionEvent) {
+    public void mnuBtnFile_OnAction(ActionEvent actionEvent) {
     }
 
     public void mnuBtnAbout_OnAction(ActionEvent actionEvent) {
     }
-
 
 
 
